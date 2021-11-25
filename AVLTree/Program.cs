3d2696bfe55c1a346f09eb3a59bddde020a21fb9
Habobiku <gireygame@gmail.com>
2021-11-26 02:21:51 +0300

    #

--- AVLTree/Program.cs
+++ AVLTree/Program.cs
@@ -7,8 +7,12 @@ namespace AVLTree
     {
         static void Main(string[] args)
         {
+            int n = 1;
             Console.WriteLine("hui");
-            
+            if(n==1)
+            {
+                Console.WriteLine("sosi");
+            }
         }
     }
 }
